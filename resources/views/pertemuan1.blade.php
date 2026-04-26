fcb27d7c4f97ec09ca453472e5b3b7ddf1843d2b
WINDOWS10 <ghifarypranaja@gmail.com>
2026-04-26 21:20:47 +0700

    tugas pertemuan 7

---
+++ resources/views/pertemuan1.blade.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>50262411109 - Ghifary Pranaja Khawiswara</title>
+</head>
+
+<body>
+    <h1>Selamat Datang di Web Personal Saya</h1>
+    <p>
+        This paragraph
+contains a lot of lines and spaces
+in the source code,
+<br>but the browser
+ignores it.
+    </p>
+    <p>
+        <img src="https://www.its.ac.id/wp-content/uploads/2026/01/Hi-me-2025-1-1-1024x576.jpg"
+        width="720px"
+        >
+    </p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ Route::get('pert5', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('pert1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('pert2part1', function () {
+    return view('pertemuan2news');
+});
+
+Route::get('pert2part2', function () {
+    return view('pertemuan2news1');
+});
+
+Route::get('pert3part1', function () {
+    return view('pertemuan3contoh');
+});
+
+Route::get('pert3part2', function () {
+    return view('pertemuan3responsive');
+});
+
+Route::get('pert3part3', function () {
+    return view('pertemuan3template');
+});
+
+Route::get('pert4', function () {
+    return view('pertemuan4');
+});
+
+Route::get('pert5part2', function () {
+    return view('pertemuan5index');
+});
